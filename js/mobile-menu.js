dc9291c971e76618fd2abcc5a50f06c1b4fe393f
zg78899 <zg788990@gmail.com>
2019-09-26 14:58:05 +0900

    반응형웹 js-1

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,10 +1,41 @@
-var nav=$('.navigation');
-var btn=$('.btn-menubar');
+var viewport=window.matchMedia('(max-width: 999px)');
+var items=$('.menu-item');
+
+
+$(window).resize(function(){
+  location.reload();
+});
+
+if(viewport.matches){
+  var nav=$('.navigation');
+  var btn=$('.btn-menubar');
+  var lists=$('menu-list');
+
+  items.addClass('icon-plus');
+  items.attr('role','button');
+  items.attr('aria-haspopup','true');
+  items.attr('aria-expanded','false');
+  items.attr('aria-pressed','false');
 
 // 버튼을 클릭했을 때 네비게이션 요소에  nav-act 클래스를 추가한다.
 btn.on('click',function(e){
   e.preventDefault();
   nav.toggleClass('nav-act');
+  if(nav.hasClass('nav-act')){
+    btn.attr('aria-label','메인메뉴 닫기');
 
-
+  }else{
+    btn.attr('aria-label','메인메뉴 열기');
+  }
 });
+  items.on('click',function(e){
+    
+    lists.removeClass('menu-act');
+   $(this).parent().addClass('menu-act');
+  });  
+}else{
+  items.attr('role','presentation');
+
+}
+
+
