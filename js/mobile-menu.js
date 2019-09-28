796865db5d074e0543f17bb4eaa5389216eca2b5
zg78899 <zg788990@gmail.com>
2019-09-28 16:25:33 +0900

    반응형 웹 최종완성본

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -38,6 +38,7 @@ btn.on('click',function(e){
 
     lists.removeClass('menu-act');
    $(this).parent().addClass('menu-act');
+
    if(lists.hasClass('menu-act')){
      items.removeClass('icon-minus').addClass('icon-plus');
      items.attr('aria-expanded','false').attr('aria-pressed','flase');
@@ -63,5 +64,59 @@ else{
   bar.attr('aria-hidden','true');
 
 }
+// var viewport = window.matchMedia('(max-width: 999px)');
+// var items = $('.menu-item');
+// var board_list = $('.board-list > li');
+// $(window).resize(function(){
+//   location.reload();
+// });
+
+// board_list.attr('class', 'icon-star');
+
+// if(viewport.matches){
+//   var nav = $('.navigation');
+//   var btn = $('.btn-menubar');
+//   var menu = $('.menu');
+//   var lists = $('.menu-list');
+
+//   items.addClass('icon-plus');
+//   items.attr('role', 'button');
+//   items.attr('aria-haspopup', 'true');
+//   items.attr('aria-expanded', 'false');
+//   items.attr('aria-pressed', 'false');
+//   btn.on('click', function(e){
+//     e.preventDefault();
+//     nav.toggleClass('nav-act');
+//     if(nav.hasClass('nav-act')){
+//       btn.attr('aria-label', '메인메뉴 닫기');
+//     }else{
+//       btn.attr('aria-label', '메인메뉴 열기');
+//     }
+//   });
+
+//   items.on('click', function(e){
+//     lists.removeClass('menu-act');
+//     $(this).parent().addClass('menu-act');
+//     if(lists.hasClass('menu-act')){
+//       items.removeClass('icon-minus').addClass('icon-plus');
+//       items.attr('aria-expanded', 'false').attr('aria-pressed', 'false')
+//       $(this).removeClass('icon-plus').addClass('icon-minus');
+//       $(this).attr('aria-expanded', 'true').attr('aria-pressed', 'true');
+//     }
+//   });
+
+//   menu.on('focusin', function(){
+//     nav.addClass('nav-act');
+//   });
+//   menu.on('focusout', function(){
+//     nav.removeClass('nav-act');
+//   });
+
+// }else{
+//   var bar = $('.bar');
+//   items.attr('role', 'presentation');
+//   bar.attr('aria-hidden', 'true');
+// }
+
 
 
